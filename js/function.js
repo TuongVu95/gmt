02509d5aab3065117e6cde9c48f3a1e37083e85a
Vu <neulvu95@gmail.com>
2022-11-08 07:22:10 +0700

    update header footer

--- js/function.js
+++ js/function.js
@@ -1,4 +1,8 @@
 const pageHome = () => {
+  const navMobile = $('.nav-mb');
+  const navClose = $('.js-nav-close'), mask = $('.js-mask');
+  const menuMb = $('.js-menu-mb');
+
   var swiper = new Swiper(".js-slide-banner", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -10,6 +14,16 @@ const pageHome = () => {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+  });
+
+  menuMb.click(function(){
+    navMobile.addClass('open');
+    $('html,body').addClass('disable-scroll');
+  });
+
+  navClose.click(function(){
+    navMobile.removeClass('open');
+    $('html,body').removeClass('disable-scroll');
   });
 }
 
